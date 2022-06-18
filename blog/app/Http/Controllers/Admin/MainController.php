2950bb4369e36b401cda3b173618ffca6ec0661c
Danya <kloveazaks@gmail.com>
2022-06-18 19:43:31 +0300

    add layouts ...

--- blog/app/Http/Controllers/Admin/MainController.php
+++ blog/app/Http/Controllers/Admin/MainController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+//use Illuminate\Support\Str;
 
 class MainController extends Controller
 {
 
     public function index()
     {
+
+
         return view('admin.index');
     }
 
